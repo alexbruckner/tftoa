831153c89a58016d41d826fc6702dbd3cccd9f97
Alex Bruckner <bruckner.alex@gmail.com>
2018-07-27 16:40:34 +0200

    initial commit with example PNG creation script

---
+++ tftoa.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import io
+import logging
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+from pngcanvas import *
+
+HEIGHT = WIDTH = 512
+
+logging.debug("Creating canvas: %d, %d", WIDTH, HEIGHT)
+c = PNGCanvas(WIDTH, HEIGHT, color=(0xff, 0, 0, 0xff))
+c.rectangle(0, 0, WIDTH - 1, HEIGHT - 1)
+
+logging.debug("Generating gradient...")
+c.vertical_gradient(1, 1, WIDTH - 2, HEIGHT - 2,
+    (0xff, 0, 0, 0xff), (0x20, 0, 0xff, 0x80))
+
+logging.debug("Drawing some lines...")
+c.color = bytearray((0, 0, 0, 0xff))
+c.line(0, 0, WIDTH - 1, HEIGHT - 1)
+c.line(0, 0, WIDTH / 2, HEIGHT - 1)
+c.line(0, 0, WIDTH - 1, HEIGHT / 2)
+
+with open("try_pngcanvas.png", "wb") as png_fil:
+    logging.debug("Writing to file...")
+    png_fil.write(c.dump())
+
+os.system('xdg-open try_pngcanvas.png')
+
